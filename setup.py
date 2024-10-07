77df092aa758b5bc170dfc622cf33e11dca680ef
Hieu Doan <hieu.a.doan@gmail.com>
2024-10-07 12:10:23 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='WQI-calculator',
+	version='0.1.0',
+	description='A Python package to calculate Water Quality Index (WQI)',
+	author='Hieu A. Doan',
+	packages=find_packages(),
+	install_requires=[]
+)
